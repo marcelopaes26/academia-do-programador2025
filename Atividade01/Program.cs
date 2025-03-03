abe2214aedc345007777f52807f15271b53859eb
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-03-03 19:24:15 -0300

    Atividades Academia do Programador

---
+++ Atividade01/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+public class Atividade01
+{
+
+    public static string MostrarMenu()
+    {
+
+        Console.WriteLine("----------- MENU -----------");
+        Console.WriteLine("1. VERIFICAR TRIÂNGULO");
+        Console.WriteLine("2. SAIR DO PROGRAMA");
+        Console.WriteLine("----------------------------");
+
+        Console.Write("Digite sua opção: ");
+        string opcao = Console.ReadLine();
+
+        return opcao;
+    }
+
+    public static double ValidaEntrada(string entrada)
+    {
+        double valor;
+        bool valido;
+
+        do
+        {         
+            Console.Write(entrada);
+            valido = double.TryParse(Console.ReadLine().Replace(".",","), out valor);
+
+            if(!valido || valor <= 0)
+            {
+                Console.WriteLine("Entrada inválida. Digite um número válido e positivo!");
+            }
+
+
+        } while (!valido || valor <= 0);
+
+        return valor;
+
+    }
+
+    public static void ValidaTriangulo()
+    {
+        double x = ValidaEntrada("Digite o 1º lado: ");
+        double y = ValidaEntrada("Digite o 2º lado: ");
+        double z = ValidaEntrada("Digite o 3º lado: ");
+
+        if ((x + y > z) && (y + z > x) && (x + z > y))
+        {
+            ValidaTipoTriangulo(x, y, z);
+        }
+        else
+        {
+            Console.WriteLine("Triângulo inválido!");
+        }
+
+
+    }
+
+    public static void ValidaTipoTriangulo(double x, double y, double z)
+    {
+        if ((x == y) && (y == z))
+        {
+            Console.WriteLine("Triângulo Equilátero!");
+        }
+        else if ((x == y) || (y == z) || (x == z))
+        {
+            Console.WriteLine("Triângulo Isósceles!");
+        }
+        else
+        {
+            Console.WriteLine("Triângulo Escaleno!");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        int opcao = 0;
+
+        while (opcao != 2)
+        {
+
+            // Converter entrada string para inteiro
+            if (int.TryParse(MostrarMenu(), out opcao))
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        ValidaTriangulo();
+                        break;
+                    case 2:
+                        Console.WriteLine("Saindo do programa... Até logo!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Digite um número!");
+            }
+
+        }
+
+    }
+
+}
---
+++ Atividade02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+public class Atividade02
+{
+
+    private static Random rnd = new Random();
+
+    public static int JogarDado()
+    {
+        int valorDado = rnd.Next(1, 7);
+        Console.WriteLine($"Número do dado: {valorDado}");
+        if (valorDado == 6)
+        {
+            Console.WriteLine("Jogando dado extra...");
+            valorDado += JogarDado();
+        }
+        return valorDado;
+    }
+
+    public static int EventosEspeciais(int posicao)
+    {
+        List<int> avanco = new List<int> {5, 10, 15};
+        List<int> recuo = new List<int> {7, 13, 20};
+
+        if (avanco.Contains(posicao))
+        {
+            posicao += 3;
+            Console.WriteLine("Avançou 3 casas!");
+        }
+        else if (recuo.Contains(posicao))
+        {
+            posicao -= 2;
+            Console.WriteLine("Recuou 2 casas!");
+        }
+        return posicao;
+    }
+
+    public static void Main(string[] args)
+    {
+        int jogador = 0, computador = 0, rodada = 1;
+        const int tamanhoPista = 30;
+
+        do
+        {
+            Console.WriteLine($"------- RODADA {rodada} -------");
+            Console.WriteLine("Turno do jogador");
+            while (true)
+            {
+                Console.WriteLine("Aperte Enter para jogar o dado...");
+                var entrada = Console.ReadKey(intercept: true);
+
+                if (entrada.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                Console.WriteLine("Tecla inválida. Tente novamente!");
+            }
+            jogador += JogarDado();
+            jogador = EventosEspeciais(jogador);
+            Console.WriteLine($"Posição do jogador: {jogador}");
+            Console.WriteLine();
+            Console.WriteLine("Turno do computador");
+            computador += JogarDado();
+            computador = EventosEspeciais(computador);
+            Console.WriteLine($"Posição do computador: {computador}");
+            Console.WriteLine();
+            rodada++;
+
+        } while ((jogador < tamanhoPista) && (computador < tamanhoPista));
+
+        if (jogador > computador)
+        {
+            Console.WriteLine($"O jogador foi campeão!\nResultado final:\nJogador = {jogador}\nComputador = {computador}");
+        }
+        else if (computador > jogador)
+        {
+            Console.WriteLine($"O computador foi campeão!\nResultado final:\nComputador = {computador}\nJogador = {jogador}");
+        }
+        else
+        {
+            Console.WriteLine($"O jogo terminou empatado!\nResultado final:\nJogador = {jogador}\nComputador = {computador}");
+        }
+
+    }
+}

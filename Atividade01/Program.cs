5d00d574c957c5c47aaa178d884997a91e33d456
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-03-03 23:21:53 -0300

    Comentários importantes

--- Atividade01/Program.cs
+++ Atividade01/Program.cs
@@ -3,6 +3,7 @@
 public class Atividade01
 {
 
+    // Função para mostrar um menu interativo com o usuário
     public static string MostrarMenu()
     {
 
@@ -17,6 +18,7 @@ public class Atividade01
         return opcao;
     }
 
+    // Função para validar a entrada de dados garantindo que esta entrada seja um número
     public static double ValidaEntrada(string entrada)
     {
         double valor;
@@ -25,6 +27,8 @@ public class Atividade01
         do
         {         
             Console.Write(entrada);
+            // Utilizando o método TryParse para converter a entrada string em valor do tipo double (real)
+            // E o Replace para garantir caso o usuário digite "." seja substiuído por "," 
             valido = double.TryParse(Console.ReadLine().Replace(".",","), out valor);
 
             if(!valido || valor <= 0)
@@ -32,21 +36,28 @@ public class Atividade01
                 Console.WriteLine("Entrada inválida. Digite um número válido e positivo!");
             }
 
-
+        // Condição do loop para garantir que a entrada seja válida ou um número positivo
+        // Ou seja enquanto o usuário não digitar uma entrada válida ou um número positivo
+        // Continuará solicitando a entrada para o usuário
         } while (!valido || valor <= 0);
 
         return valor;
 
     }
 
+    // Função para validar um triângulo de acordo com o enunciado
     public static void ValidaTriangulo()
     {
+        // Cria as variáves x, y e z e chama a função ValidaEntrada para cada uma passando um texto como parâmetro
         double x = ValidaEntrada("Digite o 1º lado: ");
         double y = ValidaEntrada("Digite o 2º lado: ");
         double z = ValidaEntrada("Digite o 3º lado: ");
 
+        // Compara se a soma de quaisquer dos lados é maior que o terceiro lado
+        // para garantir que seja um triângulo válido
         if ((x + y > z) && (y + z > x) && (x + z > y))
         {
+            // Chama a função ValidaTipoTriangulo e passa as variáveis x, y e x como parâmetros
             ValidaTipoTriangulo(x, y, z);
         }
         else
@@ -57,46 +68,57 @@ public class Atividade01
 
     }
 
+    // Função para validar qual tipo do triângulo (Equilátero, Isósceles ou Escaleno)
     public static void ValidaTipoTriangulo(double x, double y, double z)
     {
+        // Verifica se todos os lados são iguais
         if ((x == y) && (y == z))
         {
             Console.WriteLine("Triângulo Equilátero!");
         }
+        // Verifica se pelo menos dois lados são iguais
         else if ((x == y) || (y == z) || (x == z))
         {
             Console.WriteLine("Triângulo Isósceles!");
         }
+        // Caso se todos os lados forem diferentes
         else
         {
             Console.WriteLine("Triângulo Escaleno!");
         }
     }
 
+    // Ponto de entrada da aplicação C#
     public static void Main(string[] args)
     {
 
         int opcao = 0;
 
+        // Condição para que o loop continue ativo até o usuário digitar 2
         while (opcao != 2)
         {
 
-            // Converter entrada string para inteiro
+            // Converter entrada string para inteiro, parecido com a função ValidaEntrada()
+            // para garantir que o usuário não digite texto
             if (int.TryParse(MostrarMenu(), out opcao))
             {
                 switch (opcao)
                 {
                     case 1:
+                        // Caso o usuário digite 1 chama a função ValidaTriangulo()
                         ValidaTriangulo();
                         break;
                     case 2:
+                        // Caso o usuário digite 2 encerra o loop
                         Console.WriteLine("Saindo do programa... Até logo!");
                         break;
                     default:
+                        // Caso o usuário digite qualquer número diferente de 1 ou 2
                         Console.WriteLine("Opção inválida. Tente novamente!");
                         break;
                 }
             }
+            // Se o usuário digitar texto
             else
             {
                 Console.WriteLine("Entrada inválida. Digite um número!");
--- Atividade02/Program.cs
+++ Atividade02/Program.cs
@@ -3,33 +3,46 @@
 public class Atividade02
 {
 
+    // Cria um objeto Random para gerar números aleatórios
     private static Random rnd = new Random();
 
     //Função para jogar o dado a cada rodada
     public static int JogarDado()
     {
+        // Cria a variável valorDado atribuindo o valor da instância rnd utilizando a função Next
+        // passando os parâmetros 1 e 7 para gerar números aleatórios de 1 a 6
         int valorDado = rnd.Next(1, 7);
         Console.WriteLine($"Número do dado: {valorDado}");
+
+        // Verifica se o número do dado é 6 para o jogador ou computador ter uma rodada extra
         if (valorDado == 6)
         {
             Console.WriteLine("Jogando dado extra...");
+            // Se o valor do dado for 6 é chamada a função JogarDado() de forma recursiva 
+            // e somado ao valor do dado inicial
             valorDado += JogarDado();
         }
         return valorDado;
     }
 
+    // Funçao para os eventos especiais (avanço e recuo)
     public static int EventosEspeciais(int posicao)
     {
+        // Criada duas listas (avanco e recuo) para adicionar seus respectivos valores condicionais
         List<int> avanco = new List<int> {5, 10, 15};
         List<int> recuo = new List<int> {7, 13, 20};
 
+        // Utiliza o método Contains para validar se a lista contém o valor em que está a posição
         if (avanco.Contains(posicao))
         {
+            // Se a posição estiver na 5, 10 ou 15 é somado + 3 a ela
             posicao += 3;
             Console.WriteLine("Avançou 3 casas!");
         }
+        // Utiliza o método Contains para validar se a lista contém o valor em que está a posição
         else if (recuo.Contains(posicao))
         {
+            // Se a posição estiver na 7, 13 ou 20 é subtraído - 2 a ela
             posicao -= 2;
             Console.WriteLine("Recuou 2 casas!");
         }
@@ -39,6 +52,8 @@ public class Atividade02
     public static void Main(string[] args)
     {
         int jogador = 0, computador = 0, rodada = 1;
+        
+        // Criada uma constante com o valor fixo 30
         const int tamanhoPista = 30;
 
         do
@@ -48,25 +63,33 @@ public class Atividade02
             while (true)
             {
                 Console.WriteLine("Aperte Enter para jogar o dado...");
+                // Utilizado o método ReadKey para capturar a tecla que o usuário digitar 
                 var entrada = Console.ReadKey(intercept: true);
 
+                // Se a tecla for Enter o loop é interrompido e avança para o próximo bloco de código
                 if (entrada.Key == ConsoleKey.Enter)
                 {
                     break;
                 }
                 Console.WriteLine("Tecla inválida. Tente novamente!");
             }
+            // O valor retornado da função JogarDado() é somado ao valor atual do jogador
             jogador += JogarDado();
+            // o valor atual do jogador é passado como parâmetro na função EventosEspeciais() e atribuído a ele mesmo
             jogador = EventosEspeciais(jogador);
             Console.WriteLine($"Posição do jogador: {jogador}");
             Console.WriteLine();
             Console.WriteLine("Turno do computador");
+            // O valor retornado da função JogarDado() é somado ao valor atual do computador
             computador += JogarDado();
+            // o valor atual do computador é passado como parâmetro na função EventosEspeciais() e atribuído a ele mesmo
             computador = EventosEspeciais(computador);
             Console.WriteLine($"Posição do computador: {computador}");
             Console.WriteLine();
+            // Incrementa o número de rodadas
             rodada++;
 
+        // Condição para continuar o loop até o jogador ou computador ter o valor maior que tamanhoPista
         } while ((jogador < tamanhoPista) && (computador < tamanhoPista));
 
         if (jogador > computador)
